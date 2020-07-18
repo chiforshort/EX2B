6b06427edee0d4ad4a49664076caca5b1ab981c8
asia chicon <chiconba@my.erau.edu>
2020-07-17 23:17:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EX2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+        }
+
+
+        public void Sports(string Player, string Equipment)
+        {
+            string player = "Player Not Entered";
+            string equipment = "Equipment Not Entered";
+            Console.WriteLine("Default Paramater");
+            Console.WriteLine(player, equipment);
+            Console.WriteLine(player);
+
+
+            
+        }
+
+    }
+
+}
